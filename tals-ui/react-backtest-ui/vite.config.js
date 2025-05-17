0f6c288d4dc8ca3b26aa40268a78a72f647a85b4
unknown <taldokh@gmail.com>
2025-05-17 11:58:03 +0300

    add hostname to allowed hosts

--- tals-ui/react-backtest-ui/vite.config.js
+++ tals-ui/react-backtest-ui/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['elev8.cs.colman.ac.il'],
+    host: true, // this allows exposure to the network (equivalent to --host)
+  },
 })
